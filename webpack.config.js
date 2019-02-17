34031d985ea5ee6a736efa30125493329339a4b6
John Mc.Queide Clemente <mcqueide@gmail.com>
2019-02-17 10:40:00 -0300

    teste

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader',
-                options: {
+                query: {
                     presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             },
